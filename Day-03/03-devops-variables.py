23820f73e21edb36b36214e9aa5a096e706d64bf
venkat <devops.bheemireddy@gmail.com>
2025-08-12 19:45:12 +1000

    Python Coding Changes

---
+++ Day-03/03-devops-variables.py
@@ -0,0 +1,11 @@
+# Define configuration variables for a web server
+server_name = "my_server"
+port = 80
+is_https_enabled = True
+max_connections = 1000
+
+# Print the configuration
+print(f"Server Name: {server_name}")
+print(f"Port: {port}")
+print(f"HTTPS Enabled: {is_https_enabled}")
+print(f"Max Connections: {max_connections}")
