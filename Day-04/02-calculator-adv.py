4c174110340dc3d0234705c7a719397e06290e71
venkat <devops.bheemireddy@gmail.com>
2025-08-13 09:17:54 +1000

    Python Coding Changes

---
+++ Day-04/02-calculator-adv.py
@@ -0,0 +1,15 @@
+def addition(num1, num2):
+    x = num1 + num2
+    return x
+
+def subtraction(num1, num2):
+    y = num1 - num2
+    return y
+
+def multiply(num1, num2):
+    z = num1 * num2
+    return z
+
+print(addition(10,5))
+print(subtraction(10,5))
+print(multiply(10,5))
