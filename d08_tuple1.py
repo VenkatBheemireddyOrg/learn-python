9c009d77c9ef9ac5da6194d9e5da4006dabd1cc8
venkat <devops.bheemireddy@gmail.com>
2025-08-16 13:38:17 +1000

    Python Coding Changes

--- d08_tuple1.py
+++ d08_tuple1.py
@@ -3,12 +3,14 @@ my_tuple = (50, 30, 40, 10, 20)
 print("my_tuple :", my_tuple)
 print("my_tuple_length :", len(my_tuple))
 
-## sort is not possible in tupple
+## tuple doesn't support sort operation
 ## my_tuple.sort()
 ## print("my_tuple_sorted :", my_tuple)
 
 element = 30 in my_tuple
 print("element is present :", element)
 
+## tuple doesn't support append operation
+## my_tuple.append(80)
 
-my_tuple.append(80)
+my_tuple.remove(30)
