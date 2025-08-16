2b7c764660847536f686233173a57a2cfef6462d
venkat <devops.bheemireddy@gmail.com>
2025-08-16 20:04:58 +1000

    Python Coding Changes

--- d02_task2_string_functions.py
+++ d02_task2_string_functions.py
@@ -1,5 +1,22 @@
-my_string = "   Python is great language  "
-print("my_string :", my_string)
+my_text = "   Python is great language  "
+print("my_text :", my_text)
 
-my_string_new = my_string.strip()
-print("my_string_new :", my_string_new)
+### strip() function removes both head and tail spaces
+my_strip = my_text.strip()
+print("my_strip :", my_strip)
+
+my_split = my_strip.split()
+print("my_split :", my_split)
+
+my_lower = my_strip.lower()
+print("my_lower :", my_lower)
+
+my_upper = my_text.upper()
+print("my_upper : ", my_upper)
+
+print("my_length :", len(my_strip))
+
+my_replace = my_text.replace("great", "super")
+print("my_replace :", my_replace)
+
+
