d551aefd108acc9063ae0b84b9cd909533ad1f98
venkat <devops.bheemireddy@gmail.com>
2025-08-16 13:33:47 +1000

    Python Coding Changes

--- d08_tuple1.py
+++ d08_tuple1.py
@@ -3,7 +3,6 @@ my_tuple = (50, 30, 40, 10, 20)
 print("my_tuple :", my_tuple)
 print("my_tuple_length :", len(my_tuple))
 
-
 # sort is not possible in tupple
 # my_tuple.sort()
 # print("my_tuple_sorted :", my_tuple)
