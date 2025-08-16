b3240c335bbf6150466e320b7161d1dd6edf3df3
venkat <devops.bheemireddy@gmail.com>
2025-08-16 18:08:05 +1000

    Python Coding Changes

--- d02_task1_string.py
+++ d02_task1_string.py
@@ -40,7 +40,7 @@ if s2 != "-":
     result = False
 
 
-if m1 and m2 and m3 and m4 and m5 and m6 and m7 and m8 and m9 not in my_list :
+if m1 or m2 or m3 or m4 or m5 or m6 or m7 or m8 or m9 not in my_list :
     result = False
 
 if m2 not in my_list :
