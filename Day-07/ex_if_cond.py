3a9f221a715751f69d92775caa133542f3bfa802
venkat <devops.bheemireddy@gmail.com>
2025-08-16 12:25:22 +1000

    Python Coding Changes

--- Day-07/ex_if_cond.py
+++ Day-07/ex_if_cond.py
@@ -3,10 +3,10 @@ import sys
 x = sys.argv[1]
 
 if x == "t2.micro":
-    print("the instance is 2 dollars cost")
+    print("the micro instance is 2 dollars cost")
 elif x == "t2.medium":
-    print("the instance is 4 dollars cost")
+    print("the medium instance is 4 dollars cost")
 elif x == "t2.large":
-    print("the instance is 6 dollars cost")
+    print("the large instance is 6 dollars cost")
 else:
     print("the requested instance is not available")
