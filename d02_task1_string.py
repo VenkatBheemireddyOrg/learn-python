2a6cf8649f3219eb82249d2c84d69bccf579962e
venkat <devops.bheemireddy@gmail.com>
2025-08-16 17:35:54 +1000

    Python Coding Changes

--- d02_task1_string.py
+++ d02_task1_string.py
@@ -3,7 +3,11 @@ mobile = "012-456-890"
 part1 = mobile[0:3]
 part2 = mobile[4:7]
 part3 = mobile[8:11]
+part4 = mobile[3:4]
+part5 = mobile[7:8]
 
 print(part1)
 print(part2)
 print(part3)
+print(part4)
+print(part5)
