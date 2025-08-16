38ec45d76c3589dc3ee2e9c2c3620cf10c00b8e0
venkat <devops.bheemireddy@gmail.com>
2025-08-16 12:50:45 +1000

    Python Coding Changes

---
+++ d08_list2.py
@@ -0,0 +1,9 @@
+my_list = [1,7,4]
+print("my_list", my_list)
+
+new_list = my_list + [2,5]
+print("new_list", new_list)
+print("new_list_sorted", new_list.sort())
+
+slice_list = new_list[0:3]
+print("slice_list", slice_list)
