278d3912282f3d22c63ef74467dc62bb8b989ac6
venkat <devops.bheemireddy@gmail.com>
2025-08-16 12:34:56 +1000

    Python Coding Changes

---
+++ d08_list1.py
@@ -0,0 +1,15 @@
+s3_bucket_list = ["david_s3_bucket", "john_s3-bucket", "greg_s3_bucket", "jim_s3_bucket"]
+print(len(s3_bucket_list))
+
+s3_bucket_list.append("doug_s3_bucket")
+print(s3_bucket_list)
+print(len(s3_bucket_list))
+
+s3_bucket_list.remove("jim_s3_bucket")
+print(s3_bucket_list)
+print(len(s3_bucket_list))
+
+
+new_s3_bucket_list = s3_bucket_list[0:2]
+print(new_s3_bucket_list)
+print(len(new_s3_bucket_list))
