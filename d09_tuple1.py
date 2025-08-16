1759063f141580709ba7fc6e2fcf860c3c0c7dbc
venkat <devops.bheemireddy@gmail.com>
2025-08-16 13:26:13 +1000

    Python Coding Changes

---
+++ d09_tuple1.py
@@ -0,0 +1,8 @@
+my_tuple = (50, 30, 40, 10, 20)
+
+my_tuple.sort()
+print("my_tuple_sorted :", my_tuple)
+print("my_tuple_length :", len(my_tuple))
+
+element = 30 in my_tuple
+print("element is present :", element)
