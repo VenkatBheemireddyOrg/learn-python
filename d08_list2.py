755853326443bb0ddb077a933ef44b30988b4dbf
venkat <devops.bheemireddy@gmail.com>
2025-08-16 13:03:06 +1000

    Python Coding Changes

--- d08_list2.py
+++ d08_list2.py
@@ -1,12 +1,14 @@
 my_list = [10,70,40, 30, 60, 90, 80]
 print("my_list", my_list)
-print("my_list_sorted", my_list.sort())
+
+my_list.sort()
+print("my_list_sorted", my_list)
 
 new_list = my_list + [20,50]
 print("new_list", new_list)
 
-new_list_sorted = new_list.sort()
-print("new_list_sorted", new_list_sorted)
+new_list.sort()
+print("new_list_sorted", new_list_sort)
 
-slice_list = new_list_sorted[0:3]
+slice_list = new_list[0:3]
 print("slice_list", slice_list)
