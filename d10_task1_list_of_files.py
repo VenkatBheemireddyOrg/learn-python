3f4467f31831ac6ec394071b4ff5210949be0349
venkat <devops.bheemireddy@gmail.com>
2025-08-17 10:01:04 +1000

    Python Coding Changes

---
+++ d10_task1_list_of_files.py
@@ -0,0 +1,7 @@
+import os
+
+folders = input("Please provide list of folder names separated by spaces :").split()
+print("folders :", folders)
+
+for folder in folders:
+    print("folder :", folder)
