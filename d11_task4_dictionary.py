b68ab0c07f15c6c79b1f90900aea5381f3b7f120
venkat <devops.bheemireddy@gmail.com>
2025-08-17 15:49:26 +1000

    Python Coding Changes

--- d11_task4_dictionary.py
+++ d11_task4_dictionary.py
@@ -1,7 +1,10 @@
-import requests
+import requests  #github api
 
+
+# github url for integration
 response = requests.get("https://api.github.com/repos/kubernetes/kubernetes/pulls")
 
+# converting json as dictionary
 complete_detail = response.json()
 
-print(response.json())
+print(complete_dtail[0])
