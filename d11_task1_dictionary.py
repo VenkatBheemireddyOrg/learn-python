7b78cf722208ab51b112f599530c566c67aeb7ba
venkat <devops.bheemireddy@gmail.com>
2025-08-17 13:44:16 +1000

    Python Coding Changes

--- d11_task1_dictionary.py
+++ d11_task1_dictionary.py
@@ -3,6 +3,11 @@ my_dict = {"name": "Ram", "age": 25, "city": "hyderabad"}
 # Accessing dictionary
 print(my_dict["name"])
 
-
+# Modifying dictionary
 my_dict["age"] = 28
 print(my_dict["age"])
+
+# Adding into dictionary
+my_dict["occupation"] = "engineer"
+print(my_dict["occupation"])
+
